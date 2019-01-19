3172e933648b7beac69fd32ba3d6ccdffc66cae0
Sergey Musikhin <musihin_sergei@mail.ru>
2019-01-20 00:31:22 +0300

    Updates

---
+++ precache-manifest.ec6f3c7844277c429dc6037ace9969d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1db18bd0c96b9105e7d",
+    "url": "/static/js/main.f1db18bd.chunk.js"
+  },
+  {
+    "revision": "4cda2b0d979db4dacf5c",
+    "url": "/static/js/1.4cda2b0d.chunk.js"
+  },
+  {
+    "revision": "f1db18bd0c96b9105e7d",
+    "url": "/static/css/main.d5264a6e.chunk.css"
+  },
+  {
+    "revision": "0b5dd5e26dc224e8970fa86ff89d0a17",
+    "url": "/index.html"
+  }
+];
